7686769eba31711b3622144aa3e9ce84c812c825
Jonathan Beliën <dev@jbelien.be>
2018-10-25 22:09:43 +0200

    Revert "Update assets/js/calendar.js"
    This reverts commit c9a096da8bc65332d874c7f3b5c0f78b4b8e2bce.

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,27 +1,26 @@
-$(document).ready(() => {
+$(document).ready(function() {
     fetch("https://proxy.sulliops.co/index.php?http://maptime.io/belgium/feed.json")
-        .then(response => response.json())
-        .then(data => {
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
             for (let i = 0; i < data.items.length; i++) {
                 const event = data.items[i];
                 const { id: link, title, date_published } = event;
-                const time = new Date(date_published);
-                const date = time.getFullYear() + "-" + (time.getMonth() + 1) + "-" + time.getDate();
+                const date = new Date(date_published);
                 const now = new Date();
-                const today = now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate();
 
                 const li = document.createElement("li");
                 const a = document.createElement("a");
 
-                $(a)
-                    .attr({
+                $(a).attr({
                         href: link,
                         target: "_blank"
                     })
                     .html(`<span class="text-info">${date.toLocaleDateString()}</span> ${title}`);
                 $(li).append(a);
 
-                if (date >= today) {
+                if (date >= now) {
                     $("#calendar-future").append(li);
                 } else {
                     $(a).addClass("text-muted");
