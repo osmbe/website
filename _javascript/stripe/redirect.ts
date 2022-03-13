22ce642b58dcf64be856c9fbfa079b521b5a0670
Jonathan Beliën <jbelien@users.noreply.github.com>
2022-03-13 09:42:25 +0100

    Migrate to TypeScript (#159)
    * 🚧 Migrate stripe.js to stripe.ts
    
    * 🚧 Migrate event.js to event.ts
    
    * Update package.json
    
    * 🚧 Include StripeJS library
    
    * Remove StripeJS from template
    
    * Update package-lock.json
    
    * Improve Stripe typing
    
    * 📦 Update bundle

---
+++ _javascript/stripe/redirect.ts
@@ -0,0 +1,55 @@
+"use strict";
+
+import { loadStripe } from '@stripe/stripe-js';
+import Stripe from 'stripe';
+
+import { createSessionPlan } from "./session/plan";
+import { createSessionDonation } from "./session/donation";
+
+export async function redirect(data: StripePlanData | StripeDonationData): Promise<void> {
+  if (typeof data === 'undefined' || typeof data !== 'object') return;
+
+  const stripe = await loadStripe(process.env.STRIPE_PUBLISHABLE_KEY, {
+    stripeAccount: process.env.STRIPE_CONNECT_ACCOUNT
+  });
+
+  document
+    .querySelectorAll("#donation-result > div")
+    .forEach(element => ((element as HTMLDivElement).style.display = "none"));
+
+  (document.querySelector(".overlay") as HTMLElement).style.display = "";
+
+  try {
+    let session: Stripe.Checkout.Session;
+    if (data.hasOwnProperty('plan') && typeof (data as StripePlanData).plan !== "undefined") {
+      session = await createSessionPlan((data as StripePlanData));
+    } else if (data.hasOwnProperty('amount') && typeof (data as StripeDonationData).amount !== "undefined") {
+      session = await createSessionDonation((data as StripeDonationData));
+    }
+
+    if (typeof session === 'undefined') {
+      throw 'Unable to initialize Stripe session';
+    }
+
+    if (session.hasOwnProperty('error')) {
+      throw (session as any).error;
+    }
+
+    stripe
+      .redirectToCheckout({
+        sessionId: session.id
+      })
+      .then(result => {
+        if (result.error) {
+          throw result.error.message;
+        }
+      });
+  } catch (error) {
+    (document.querySelector(".overlay") as HTMLElement).style.display = "none";
+
+    document.getElementById("error-message").innerText = error;
+    document.getElementById("donation-result-error").style.display = "";
+
+    document.getElementById("donation-result-error").scrollIntoView();
+  }
+}
