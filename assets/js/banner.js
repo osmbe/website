dedfe9e0026f2a0debc780fbb652c2a85d427041
Jonathan Beliën <jbelien@users.noreply.github.com>
2024-12-25 18:44:17 +0000

    Apply linting

--- assets/js/banner.js
+++ assets/js/banner.js
@@ -2,17 +2,16 @@ const lang = document.documentElement.lang;
 const element = document.getElementById("banner-next-event");
 
 if (typeof OSMCal !== "undefined" && element !== null) {
-    const widget = new OSMCal.WidgetList(
-        element, 
-        { 
-            limit: 1, 
-            filter: { in: "Belgium" }, 
-            locales: lang, 
-            hideCancelled: true 
-        }
-    );
+  const widget = new OSMCal.WidgetList(element, {
+    limit: 1,
+    filter: { in: "Belgium" },
+    locales: lang,
+    hideCancelled: true,
+  });
 
-    widget.setTemplate('{{ name }} &bull; {{ date.human }} &bull; {{ location.short }}');
+  widget.setTemplate(
+    "{{ name }} &bull; {{ date.human }} &bull; {{ location.short }}",
+  );
 
-    widget.display();
+  widget.display();
 }
